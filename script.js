f73e8b97d749227d23d28d2bc927ab2f15e8e902
Hoang Hai <hachai18102002@gmail.com>
2023-05-31 17:59:56 +0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const checkpassLink = document.querySelector('.checkpass-link');
 
 registerLink.addEventListener('click',()=>{
     wrapper.classList.add('active');
@@ -9,3 +10,7 @@ registerLink.addEventListener('click',()=>{
 loginLink.addEventListener('click',()=>{
     wrapper.classList.remove('active');
 });
+
+// checkpassLink.addEventListener('click',()=>{
+//     wrapper.classList.add('active');
+// });
